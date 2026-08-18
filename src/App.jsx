bf6553fd3bedd163a12f475ea04fa388a02b5d12
Asieljo <rosiles.garcia.asiel@gmail.com>
2026-08-18 16:49:19 -0600

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CaseStudy from './pages/CaseStudy'
 import Prototype from './pages/Prototype'
